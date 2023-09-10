6efacc159a33a6c518978dced716d07ac4957319
minhyung <dlalsgud12@naver.com>
2023-09-10 10:20:53 +0900

    Controller 핸들러 파라미터에서 사용하는 애노테이션

--- src/main/java/com/example/webexam/WebexamApplication.java
+++ src/main/java/com/example/webexam/WebexamApplication.java
@@ -1,9 +1,11 @@
 package com.example.webexam;
 
+import ch.qos.logback.classic.pattern.MessageConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.HandlerAdapter;
 import org.springframework.web.servlet.HandlerMapping;
 import org.springframework.web.servlet.ViewResolver;
@@ -40,10 +42,18 @@ public class WebexamApplication implements CommandLineRunner {
 	@Autowired
 	List<ViewResolver> viewResolvers;
 
+
+//	org.springframework.http.converter.StringHttpMessageConverter
+//	org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
+//	implementation 'com.fasterxml.jackson.dataformat:jackson-dataformat-xml:2.11.4' 이 라이브러리를 추가해주면
+//	org.springframework.http.converter.xml.MappingJackson2XmlHttpMessageConverter 객체를 xml로 or xml를 객체로 바꿔주는 converter가 내부적으로 사용된다.
+	@Autowired
+	List<HttpMessageConverter> messageConverters; // Converter가 JSON문자열을 객체로 변환해주게 된다.
+
 	@Override
 	public void run(String... args) throws Exception {
-		for(ViewResolver viewResolver : viewResolvers){
-			System.out.println(viewResolver.getClass().getName());
+		for(HttpMessageConverter messageConverter : messageConverters){
+			System.out.println(messageConverter.getClass().getName());
 		}
 	}
 }
--- src/main/java/com/example/webexam/controller/ExampleController.java
+++ src/main/java/com/example/webexam/controller/ExampleController.java
@@ -29,8 +29,8 @@ public class ExampleController {
     //  <input type="text" name="name" placeholder="name">
     //  <input type="text" name="email" placeholder="email">
     @PostMapping("/3")
-    public String example3(@RequestParam(name = "name") String name,
-                           @RequestParam(name = "email") String email){ // 브라우저의 요청값을 이 핸들러가 그대로 받는다.
+    public String example3(@RequestParam(name = "name", required = true) String name,
+                           @RequestParam(name = "email", required = false, defaultValue = "empty_email") String email){ // 브라우저의 요청값을 이 핸들러가 그대로 받는다.
         System.out.println(name);
         System.out.println(email);
 //        System.out.println(request.getParameter("name"));
@@ -43,11 +43,28 @@ public class ExampleController {
     // javascript가 호출할때는 json데이터를 post방식으로 body에 담아서 전송한다.
     // 백엔드개발자가 javascript를 작성해서 테스트를 해야하나?? -> javascript가 없어도 테스르틑 할 수 있는 도구가 필요하다. -> Postman
     // RestController에서 객체를 반환하면 json으로 변환된다.(헤더에 데이터를 담아서 보내면 xml이나 josn 결과가 나온다)
+    // content-type을 application/json으로 하면 요청 바디에 json데이터를 담아서 보낸다.
+    // Accept에 application/xml로 하면 리턴한 객체(User)를 xml로 변환해서 응답으로 보낸다.
     @PostMapping("/4")
     public User example4(@RequestBody User user){ // 브라우저의 요청값을 이 핸들러가 그대로 받는다.
         System.out.println(user);
         user.setRandom(new Random().nextInt(100));
         return user;
+    } // Converter가 JSON문자열을 User객체로 변환해주게 된다. 뿐만 아니라 xml, 엑셀등도 이것을 이용해서 변환 할 수 있다.
+
+    // http://localhost:9090/example/pathexam/어떤특정값
+    @GetMapping("/pathexam/{id}")
+    public String pathExam(@PathVariable(name = "id") String id){
+        System.out.println(id);
+        return "pathExam";
     }
+
+    // http://localhost:9090/example/headerexam
+    @GetMapping("/headerexam")
+    public String headerExam(@RequestHeader(name = "User-Agent") String userAgentValue){
+        System.out.println(userAgentValue);
+        return "headerexam";
+    }
+
 
 }
